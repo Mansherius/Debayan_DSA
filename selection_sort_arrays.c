51704b6ae54942e29d217f0767a659ab5631954d
unknown <mansherius@gmail.com>
2022-11-13 19:53:03 +0530

    working selection sort based on FCP logic

--- selection_sort_arrays.c
+++ selection_sort_arrays.c
@@ -28,15 +28,52 @@ void printarr(int * a, int n)
 }
 
 // We require a swap function to use in the program
-void swap(int *x, int *y) {
+void swap(int *x, int *y)
+{
 	int temp;
 	temp = *x;
 	*x = *y;
 	*y = temp;
 }
 
-// Now let us write the insertion sort algorithm
-void selection_sort(int *a)
+// We also require to find the minimum element in the array
+int FindMinElement(int * a, int i, int n)
 {
+    int min = i;
+    for (int j = i+1; j < n; j++)
+    {
+        if (a[j] < a[min])
+        {
+            min = j;
+        }
+    }
+    return min;
+}
 
+// Now let us write the insertion sort algorithm
+void selection_sort(int * a, int n)
+{
+	int i, mini;
+
+	for(i=0; i<n; i++)
+    {
+		mini = FindMinElement(a, i, n);
+		swap( &a[mini], &a[i] ); // swap the values present at the addresses
+	}
+}
+
+int main()
+{
+    int n;
+    srand(time(NULL));
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    int * a = genrandarr(n);
+    printf("The array is: ");
+    printarr(a, n);
+    selection_sort(a, n);
+    printf("The sorted array is: ");
+    printarr(a, n);
+    free(a);
+    return 0;
 }
