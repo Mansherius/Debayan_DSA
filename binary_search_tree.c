6615199ffbd0a5f1973b422b2808446544849ead
unknown <mansherius@gmail.com>
2022-11-14 03:30:27 +0530

    corrected version of Binary Trees without searching for a specific value's existence in the Binary Tree

--- binary_search_tree.c
+++ binary_search_tree.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
 #define COUNT 10
 
 struct node
@@ -11,6 +13,21 @@ struct node
 
 typedef struct node TNode;
 
+// Let us now generate a random array for the Binary Search Tree
+int * genrandarr(int n)
+{
+    int * arr = malloc(n * sizeof(int));
+    if(arr)
+    {
+        for (int i = 0; i < n; i++)
+            {
+                arr[i] = rand() % 10000;
+            }
+    }
+    return arr;
+}
+
+
 // traversing the tree in order as this makes the most sense to me
 void PreOrder(struct node* root) {
   if (root == NULL) return;
@@ -20,60 +37,69 @@ void PreOrder(struct node* root) {
 }
 
 // Create a new Node
-TNode* MakeANode(value)
+TNode* MakeANode(val)
 {
     TNode* NewNode = malloc(sizeof(TNode));
-    NewNode->data = value;
+    NewNode->data = val;
     NewNode->left = NULL;
     NewNode->right = NULL;
 
     return NewNode;
 }
 
-// Inserting data on the left side of the node
-TNode* insertLeft(TNode* root, int value)
+
+// Function to insert data into a specific binary search tree
+
+TNode* InsertInTree(TNode* root, int data)
 {
-    root->left = MakeANode(value);
-    return root->left;
+
+	if(root==NULL) return MakeANode(data);
+
+	if (data < root->data)
+    {
+        root->left = InsertInTree(root->left , data);
+    }
+
+	else if (data > root->data)
+    {
+        root->right = InsertInTree(root->right, data);
+    }
+	return root;
 }
 
-// Inserting data on the right side of the node
-TNode* insertRight(TNode* root, int value)
+
+TNode* genTree(TNode* root, int* a, int n)
 {
-    root->right = MakeANode(value);
-    return root->right;
+    // Inserting values into the tree based on the randomly generated array
+    for (int i = 0; i < n; i++)
+    {
+        root = InsertInTree(root, a[i]);
+    }
+    return root;
 }
 
 
 // Code based on snippet from geeksforgeeks to print 2D model
-void print2DUtil(TNode* root, int space)
+void PrintTree2D(TNode* root, int space)
 {
     // Base case
     if (root == NULL)
         return;
  
-    // Increase distance between levels
+    // Increasing the distance between levels to form that req. negative space
     space += COUNT;
  
-    // Process right child first
-    print2DUtil(root->right, space);
- 
-    // Print current node after space
-    // count
+    // Processing the right child first
+    PrintTree2D(root->right, space);
+
+    // Printing the current node with required spaces
     printf("\n");
     for (int i = COUNT; i < space; i++)
         printf(" ");
     printf("%d\n", root->data);
  
-    // Process left child
-    print2DUtil(root->left, space);
-}
- 
-// Wrapper over print2DUtil()
-void print2D(TNode* root)
-{
-    // Pass initial space count as 0
-    print2DUtil(root, 0);
+    // Now we process the left child recursively
+    PrintTree2D(root->left, space);
 }
 
 // ! Function to free memory allocated to a BST.
@@ -82,15 +108,31 @@ void FreetheTree(TNode* root)
 	free(root);
 }
 
-int main()
+int main(int argc, char **argv)
 {
-    TNode* root = MakeANode(1);
-    insertLeft(root, 2);
-    insertRight(root, 3);
-    insertLeft(root->left, 4);
+    int * a;
+    int n;
+    printf("How many elements should be in the Binary Tree? \n");
+    scanf("%d", &n);
 
-    printf("Traversing the tree in Order: \n");
-    print2D(root);
-    FreetheTree(root);
+    srand(time(NULL));
+    a = genrandarr(n);
+    /*
+    This piece of code was to check if the Binary tree was forming correctly 
+    When using random values, I was getting degenerate trees and some trees that looked like complete nonsense
+    To check if code is indeed correct, you can comment out the random array
+    and use this predefined tester array instead:
+
+    int a[] = {4, 2, 1, 3, 8, 6, 13}; // Array to be used to generate BST
+	int n = 7;
+    */
+
+    TNode* root = NULL;
+    root = genTree(root, a, n);
     
+    PrintTree2D(root, 0);
+    
+    FreetheTree(root);
+    free(a);
+    return 0;
 }
