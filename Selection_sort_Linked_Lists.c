1f45a5cfa6179e1918ada8281138b4efceb4c123
unknown <mansherius@gmail.com>
2022-11-21 11:15:25 +0530

    Selection sort for Linked Lists with use specified array length

---
+++ Selection_sort_Linked_Lists.c
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+// We are including time.h to use the time function to generate random numbers
+
+// Simply updating code from array_linked_lists.c and selection_sort_arrays.c
+// First we define the structure of the node
+struct nodeType
+{
+    int data;
+    struct nodeType* next;
+};
+
+typedef struct nodeType Node;
+
+// Let us now generate a random array for the linked list
+int * genrandarr(int n)
+{
+    int * arr = malloc(n * sizeof(int));
+    if(arr)
+    {
+        for (int i = 0; i < n; i++)
+            {
+                arr[i] = rand() % 1000;
+            }
+    }
+    return arr;
+}
+
+// To generate a Linked list from the above array
+Node* genLinkedfromArr(int * a, int n) {
+	int i;
+    // Initialising the head and temp nodes
+	Node* head = NULL;
+	Node* temp = NULL;
+
+	if(n>0)
+    {
+		head = malloc( sizeof(Node) );
+		head->data = a[0];
+		head->next = NULL;
+		temp = head;
+	}
+
+
+	for(i=1; i<n; i++)
+    {
+		temp->next = malloc( sizeof(Node) );
+		temp = temp->next;
+		temp->data = a[i];
+		temp->next = NULL;
+	}
+
+	return head;
+}
+
+// Freeing the space malloc'd for the linked list
+void freeLinkedList(Node* head) {
+	Node* prev;
+	while(head) {
+		prev = head;
+		head = head->next;
+		free(prev);
+	}
+}
+
+// To print out the array
+void printArray(int * a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+}
+
+// To print out the linked list
+void PrintingTheList(Node* head) {
+    Node *temp = head;
+    int i = 0;
+    while(temp != NULL){
+        printf(i?" -> %d":"%d", temp->data);
+        temp = temp->next;
+        i++;
+    }
+    printf("\n");
+}
+
+// Now lets us write the selection sort algorithm for the linked list
+void selectionSort(Node* head)
+{
+    Node *temp = head;
+    Node *min = head;
+    Node *temp2 = head;
+    int tempdata;
+    while(temp != NULL)
+    {
+        // Original assumption is that the first element is the minimum
+        min = temp;
+        // Setting the value of temp2 to the next element so that we can compare it with the minimum
+        // and change pointers if required
+        temp2 = temp->next;
+
+        // Finding the minimum element in the linked list
+        while(temp2 != NULL)
+        {
+            // If the value of the next element is less than the value of the minimum element
+            // then we change the value of the minimum element
+            if(temp2->data < min->data)
+            {
+                min = temp2;
+            }
+            temp2 = temp2->next;
+        }
+        // Now we swap the order of the minimum element and the current element
+        tempdata = temp->data;
+        temp->data = min->data;
+        min->data = tempdata;
+        temp = temp->next;
+    }
+}
+
+// Main code
+int main(int argc, char **argv) {
+	int * a;
+    int n;
+
+    // Taking the input from the user
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+	Node* list;
+    // We are using the time function to generate random numbers
+	srand(time(NULL));
+
+	a = genrandarr(n);
+	list = genLinkedfromArr(a,n);
+
+    printf("Your array is: \n");
+	printArray(a,n);
+    PrintingTheList(list);
+
+    printf("Your sorted array is: \n");
+    selectionSort(list);
+    PrintingTheList(list);
+
+	free(a);
+	freeLinkedList(list);
+
+	return 0;
+}
